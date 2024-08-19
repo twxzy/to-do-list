fe2ce3141c6bbc3841c0d3060ee1a18a2c2dbdbf
Teexy360 <klebersomoliveira90@gmail.com>
2024-08-18 23:03:32 -0300

    task-remover-button

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const add = document.querySelector('.add');
 let ul = document.getElementById('list');
 
+
 /*EVENT THAT CALLS NEWADD() WHEN PRESSING "ENTER"*/
-let i = parseInt(localStorage.getItem('i')) || 1;
+let i = parseInt(localStorage.getItem('i')) || 0;
 
 add.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
+
         event.preventDefault();
         newadd();
     }
@@ -14,12 +16,12 @@ add.addEventListener('keydown', function(event) {
 /*FUNCTION THAT CREATES LISTS AND STORES IN LOCALSTORAGE AND UPDATE WITHIN THE UL*/
 function newadd() {
     if (i <= 30) {
-        let li = `<li id="inputsList">${add.value}</li>`;
+        i++;
+        let li = `<li id="inputsList"><div>${add.value}</div><div><i onclick="test(${i})"  id="trashed" class="bi trash bi-trash3"></i></div></li>`;
         add.value = '';
 
         localStorage.setItem('item' + i, li);
         
-        i++;
         localStorage.setItem('i', i);
 
         ul.insertAdjacentHTML('beforeend', li);
@@ -28,12 +30,24 @@ function newadd() {
     }
 }
 
+/*TASK REMOVER*/
+function test(a){
+    localStorage.removeItem('item' + a)
+    document.getElementById('inputsList').remove()
+    console.log(a)
+
+    let ind = localStorage.getItem('i')
+    ind -= 1
+    localStorage.setItem('i', ind)
+}
+
 /*LOAD ALL ITEMS FROM LOCALSTORAGE AND DISPLAY ALL LI*/
 document.addEventListener('DOMContentLoaded', function() {
+    
     for (let j = 1; j < i; j++) {
         let savedItem = localStorage.getItem('item' + j);
-        if (savedItem) {
-            ul.insertAdjacentHTML('beforeend', savedItem);
+            if (savedItem) {
+                ul.insertAdjacentHTML('beforeend', savedItem);
+            }
         }
-    }
 });
